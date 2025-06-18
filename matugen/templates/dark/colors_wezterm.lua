99a349cef280e84f3798115f4ffa34eac8bb799b
alexander <alexander6432@gmail.com>
2025-06-18 14:40:15 -0600

    se tradujo yazi

--- matugen/templates/dark/colors_wezterm.lua
+++ matugen/templates/dark/colors_wezterm.lua
@@ -1,8 +1,8 @@
 return {
 	-- ======= DARK ======
 	-- ===== PRIMARY =====
-	["primary_dark"] = "{{ colors.primary.dark.hex | set_lightness: -10.0 }}", --cyan
-	["primary"] = "{{ colors.primary.dark.hex }}", --lightcyan
+	["primary_dark"] = "{{ colors.primary.dark.hex | set_lightness: -10.0 }}",
+	["primary"] = "{{ colors.primary.dark.hex }}",
 	["primary_container"] = "{{ colors.primary_container.dark.hex }}",
 	["primary_fixed"] = "{{ colors.primary_fixed.dark.hex }}",
 	["primary_fixed_dim"] = "{{ colors.primary_fixed_dim.dark.hex }}",
@@ -13,8 +13,8 @@ return {
 	["inverse_primary"] = "{{ colors.inverse_primary.dark.hex }}",
 
 	-- ===== PRIMARY HUE300 =====
-	["primary_dark_hue300"] = "{{ colors.primary.dark.hex | set_hue: 300.0 | set_lightness: -10.0 }}", --green
-	["primary_hue300"] = "{{ colors.primary.dark.hex | set_hue: 300.0 }}", --lightgreen
+	["primary_dark_hue300"] = "{{ colors.primary.dark.hex | set_hue: 340.0 | set_lightness: -10.0 }}",
+	["primary_hue300"] = "{{ colors.primary.dark.hex | set_hue: 340.0 }}",
 	["primary_container_hue300"] = "{{ colors.primary_container.dark.hex | set_hue: 300.0 }}",
 	["primary_fixed_hue300"] = "{{ colors.primary_fixed.dark.hex | set_hue: 300.0 }}",
 	["primary_fixed_dim_hue300"] = "{{ colors.primary_fixed_dim.dark.hex | set_hue: 300.0 }}",
@@ -25,8 +25,8 @@ return {
 	["inverse_primary_hue300"] = "{{ colors.inverse_primary.dark.hex | set_hue: 300.0 }}",
 
 	-- ===== PRIMARY HUE240 =====
-	["primary_dark_hue240"] = "{{ colors.primary.dark.hex | set_hue: 240.0 | set_lightness: -10.0 }}", --yellow
-	["primary_hue240"] = "{{ colors.primary.dark.hex | set_hue: 240.0 }}", --lightyellow
+	["primary_dark_hue240"] = "{{ colors.primary.dark.hex | set_hue: 320.0 | set_lightness: -10.0 }}",
+	["primary_hue240"] = "{{ colors.primary.dark.hex | set_hue: 320.0 }}",
 	["primary_container_hue240"] = "{{ colors.primary_container.dark.hex | set_hue: 240.0 }}",
 	["primary_fixed_hue240"] = "{{ colors.primary_fixed.dark.hex | set_hue: 240.0 }}",
 	["primary_fixed_dim_hue240"] = "{{ colors.primary_fixed_dim.dark.hex | set_hue: 240.0 }}",
@@ -37,8 +37,8 @@ return {
 	["inverse_primary_hue240"] = "{{ colors.inverse_primary.dark.hex | set_hue: 240.0 }}",
 
 	-- ===== PRIMARY HUE180 =====
-	["primary_dark_hue180"] = "{{ colors.primary.dark.hex | set_hue: 180.0 | set_lightness: -10.0 }}", --red
-	["primary_hue180"] = "{{ colors.primary.dark.hex | set_hue: 180.0 }}", --lightred
+	["primary_dark_hue180"] = "{{ colors.primary.dark.hex | set_hue: 300.0 | set_lightness: -10.0 }}",
+	["primary_hue180"] = "{{ colors.primary.dark.hex | set_hue: 300.0 }}",
 	["primary_container_hue180"] = "{{ colors.primary_container.dark.hex | set_hue: 180.0 }}",
 	["primary_fixed_hue180"] = "{{ colors.primary_fixed.dark.hex | set_hue: 180.0 }}",
 	["primary_fixed_dim_hue180"] = "{{ colors.primary_fixed_dim.dark.hex | set_hue: 180.0 }}",
@@ -49,8 +49,8 @@ return {
 	["inverse_primary_hue180"] = "{{ colors.inverse_primary.dark.hex | set_hue: 180.0 }}",
 
 	-- ===== PRIMARY HUE120 =====
-	["primary_dark_hue120"] = "{{ colors.primary.dark.hex | set_hue: 120.0 | set_lightness: -10.0 }}", --magenta
-	["primary_hue120"] = "{{ colors.primary.dark.hex | set_hue: 120.0 }}", --lightmagenta
+	["primary_dark_hue120"] = "{{ colors.primary.dark.hex | set_hue: 40.0 | set_lightness: -10.0 }}",
+	["primary_hue120"] = "{{ colors.primary.dark.hex | set_hue: 40.0 }}",
 	["primary_container_hue120"] = "{{ colors.primary_container.dark.hex | set_hue: 120.0 }}",
 	["primary_fixed_hue120"] = "{{ colors.primary_fixed.dark.hex | set_hue: 120.0 }}",
 	["primary_fixed_dim_hue120"] = "{{ colors.primary_fixed_dim.dark.hex | set_hue: 120.0 }}",
@@ -61,8 +61,8 @@ return {
 	["inverse_primary_hue120"] = "{{ colors.inverse_primary.dark.hex | set_hue: 120.0 }}",
 
 	-- ===== PRIMARY HUE60 =====
-	["primary_dark_hue60"] = "{{ colors.primary.dark.hex | set_hue: 60.0 | set_lightness: -10.0 }}", --blue
-	["primary_hue60"] = "{{ colors.primary.dark.hex | set_hue: 60.0 }}", --lightblue
+	["primary_dark_hue60"] = "{{ colors.primary.dark.hex | set_hue: 20.0 | set_lightness: -10.0 }}",
+	["primary_hue60"] = "{{ colors.primary.dark.hex | set_hue: 20.0 }}",
 	["primary_container_hue60"] = "{{ colors.primary_container.dark.hex | set_hue: 60.0 }}",
 	["primary_fixed_hue60"] = "{{ colors.primary_fixed.dark.hex | set_hue: 60.0 }}",
 	["primary_fixed_dim_hue60"] = "{{ colors.primary_fixed_dim.dark.hex | set_hue: 60.0 }}",
@@ -93,7 +93,7 @@ return {
 	["on_tertiary_fixed_variant"] = "{{ colors.on_tertiary_fixed_variant.dark.hex }}",
 
 	-- ===== BACKGROUND & SURFACE =====
-	["background"] = "{{ colors.background.dark.hex }}", --black
+	["background"] = "{{ colors.background.dark.hex }}",
 	["surface"] = "{{ colors.surface.dark.hex }}",
 	["surface_bright"] = "{{ colors.surface_bright.dark.hex }}",
 	["surface_container"] = "{{ colors.surface_container.dark.hex }}",
@@ -104,7 +104,7 @@ return {
 	["surface_dim"] = "{{ colors.surface_dim.dark.hex }}",
 	["surface_tint"] = "{{ colors.surface_tint.dark.hex }}",
 	["surface_variant"] = "{{ colors.surface_variant.dark.hex }}",
-	["on_background"] = "{{ colors.on_background.dark.hex }}", --white
+	["on_background"] = "{{ colors.on_background.dark.hex }}",
 	["on_surface"] = "{{ colors.on_surface.dark.hex }}",
 	["on_surface_variant"] = "{{ colors.on_surface_variant.dark.hex }}",
 	["inverse_on_surface"] = "{{ colors.inverse_on_surface.dark.hex }}",
@@ -117,8 +117,8 @@ return {
 	["on_error_container"] = "{{ colors.on_error_container.dark.hex }}",
 
 	-- ===== OUTLINE =====
-	["outline"] = "{{ colors.outline.dark.hex }}", --darkgray
-	["outline_variant"] = "{{ colors.outline_variant.dark.hex }}", --gray
+	["outline"] = "{{ colors.outline.dark.hex }}",
+	["outline_variant"] = "{{ colors.outline_variant.dark.hex }}",
 
 	-- ===== SCRIM & SHADOW =====
 	["scrim"] = "{{ colors.scrim.dark.hex }}",
